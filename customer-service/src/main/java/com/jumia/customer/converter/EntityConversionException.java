9b72d094b6f4f7d50d37450226035a9e04ce4afa
tommelo <neilton@commerce-connector.com>
2022-03-01 17:45:44 +0100

    Added entity conversion exception class

---
+++ customer-service/src/main/java/com/jumia/customer/converter/EntityConversionException.java
@@ -0,0 +1,9 @@
+package com.jumia.customer.converter;
+
+public class EntityConversionException extends Exception {
+
+    public EntityConversionException(String message) {
+        super(message);
+    }
+
+}
