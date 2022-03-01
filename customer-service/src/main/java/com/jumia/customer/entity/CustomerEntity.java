8aacb555b0d1d85a39d566643915f9ddcd0d0d09
tommelo <neilton@commerce-connector.com>
2022-03-01 17:14:11 +0100

    Added customer entity

---
+++ customer-service/src/main/java/com/jumia/customer/entity/CustomerEntity.java
@@ -0,0 +1,43 @@
+package com.jumia.customer.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = CustomerEntity.TABLE_NAME)
+public class CustomerEntity {
+
+    public static final String TABLE_NAME = "customer";
+    public static final String PHONE_COLUMN_NAME = "phone";
+
+    @Id
+    private int id;
+    private String name;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
