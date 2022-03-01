463c725239a07d41d5070029ffef45b0287e7c66
tommelo <neilton@commerce-connector.com>
2022-03-01 19:51:15 +0100

    Removed error handling from customer controller

--- customer-service/src/main/java/com/jumia/customer/controller/CustomerController.java
+++ customer-service/src/main/java/com/jumia/customer/controller/CustomerController.java
@@ -6,11 +6,7 @@ import com.jumia.customer.converter.EntityConversionException;
 import com.jumia.customer.converter.EntityConverter;
 import com.jumia.customer.model.Customer;
 import com.jumia.customer.service.CustomerService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -31,21 +27,6 @@ public class CustomerController {
     public ResponseEntity<List<GetCustomersResponse>> getCustomers(GetCustomersRequest request) throws EntityConversionException {
         List<Customer> customers = customerService.findAllCustomers(request.getCountries(), request.getValid());
         return ResponseEntity.ok(entityConverter.convert(customers));
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({ BindException.class })
-    public Map<String, String> handleBindException(BindException e) {
-        Map<String, String> response = new HashMap<>();
-
-        List<ObjectError> errors = e.getBindingResult().getAllErrors();
-        for (ObjectError error : errors) {
-            String field = ((FieldError) error).getField();
-            String message = error.getDefaultMessage();
-            response.put(field, message);
-        }
-
-        return response;
     }
 
 }
