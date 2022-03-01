cf6c729ce1a0e6b86e4eb6a9e10bc683d10fd879
tommelo <neilton@commerce-connector.com>
2022-03-01 16:42:37 +0100

    Added customer model

---
+++ customer-service/src/main/java/com/jumia/customer/model/Customer.java
@@ -0,0 +1,39 @@
+package com.jumia.customer.model;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private Phone phone;
+
+    public Customer(int id, String name, Phone phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+}
